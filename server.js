5c9690482f65697b82295b0868bb84d310484390
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-07-11 20:36:08 -0500

    original commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const apiRoutes = require('./Develop/routes/apiRoutes');
+
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+const fs = require('fs');
+const path = require('path');
+
+// const { animals } = require('./data/animals');
+
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
