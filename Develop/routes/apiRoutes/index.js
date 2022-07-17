403730299ca450736a2ab8b0e014048771a67799
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-07-17 11:13:30 -0500

    notes and notes id api working

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { query } = require("express");
 // must all have the same name 'const db', 'json arry', 'db.json file'
 const { db } = require('../../../Develop/db/db');
 
@@ -9,21 +10,14 @@ const { db } = require('../../../Develop/db/db');
 //     validateZookeeper,
 //   } = require("../../lib/zookeepers");
   
-function findById(id, db) {
-    const result = db.filter((db) => db.id === id)[0];
-    return result;
-  }
-  
-// router.get("/notes", (req, res) => {
-//     let results = notes;
-//     if (req.query) {
-//         results = filterByQuery(req.query, results);
-//     }
-//     res.json(results);
-// });
+// get all notes
+router.get("/notes", (req, res) => {
+    let results = db;
+    res.json(results);
+});
 
 router.get("/notes:id", (req, res) => {
-    const result = findById(req.params.id, db);
+  const result = db.filter((db) => db.id === req.query.id)[0];
     if (result) {
       res.json(result);
     } else {
@@ -42,13 +36,18 @@ router.get("/notes:id", (req, res) => {
 
 // router.post("/notes", (req, res) => {
 //   req.body.id = notes.length.toString();
-
-//   if (!validateZookeeper(req.body)) {
-//     res.status(400).send("The not is not properly formatted.");
-//   } else {
-//     const note = createNewZookeeper(req.body, notes);
-//     res.json(notesDB);
-//   }
+//   const note = createNewNote(req.body, notes);
+//   res.json(notesDB);
 // });
+
+// function createNewNote(body, notes) {
+//   const zookeeper = body;
+//   notes.push(zookeeper);
+//   fs.writeFileSync(
+//     path.join(__dirname, "../data/zookeepers.json"),
+//     JSON.stringify({ notes }, null, 2)
+//   );
+//   return zookeeper;
+// }
 
 module.exports = router;
