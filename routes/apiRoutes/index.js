b276ab87b6b7d68eeedeffb9d98b2123632e9a92
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-07-31 08:50:54 -0500

    pages switching

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const router = require("express").Router();
+const { query } = require("express");
+
+const { v4: uuidv4 } = require("uuid");
+// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+// get all notes
+router.get("/notes", (req, res) => {
+  let results = db;
+  res.json(results);
+});
+
+router.delete("/notes:id", (req, res) => {
+  const result = db.filter((db) => db.id === req.query.id)[0];
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post("/notes", (req, res) => {
+  const note = req.body;
+  note.id = uuidv4();
+  fs.readFile("../../../Develop/db/db.json", "utf8", function (err, data) {
+    // Display the file content
+    console.log(data);
+  });
+  db.push(note);
+  fs.writeFileSync(path.join(__dirname, db), JSON.stringify({ note }, null, 2));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// const apiRoutes = require('./Develop/routes/apiRoutes');
-// const htmlRoutes = require('./Develop/routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-// const { animals } = require('./data/animals');
+const { db } = require("./db/db");
 
-const express = require('./Develop/node_modules/express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-app.use(express.static('public'));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
